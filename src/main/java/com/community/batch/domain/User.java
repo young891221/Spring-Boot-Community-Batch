21c4c9dd353fd9df5c4921dd7619c9879b7cf0ed
young891221 <young891221@gmail.com>
2018-02-23 18:51:23 +0900

    init domain

---
+++ src/main/java/com/community/batch/domain/User.java
@@ -0,0 +1,67 @@
+package com.community.batch.domain;
+
+
+import com.community.batch.domain.enums.SocialType;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by KimYJ on 2017-07-12.
+ */
+@Getter
+@NoArgsConstructor
+@Entity
+@Table
+public class User implements Serializable {
+
+    @Id
+    @Column
+    @GeneratedValue
+    private Long idx;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @Column
+    private String pincipal;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updatedDate;
+
+    @Builder
+    public User(String name, String password, String email, String pincipal, SocialType socialType, LocalDateTime createdDate, LocalDateTime updatedDate) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.pincipal = pincipal;
+        this.socialType = socialType;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+}
---
+++ src/main/java/com/community/batch/domain/enums/BoardType.java
@@ -0,0 +1,19 @@
+package com.community.batch.domain.enums;
+
+/**
+ * Created by KimYJ on 2017-07-12.
+ */
+public enum BoardType {
+    notice("공지사항"),
+    free("자유게시판");
+
+    private String value;
+
+    BoardType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}
