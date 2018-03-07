a86d98304d2ccbea5ca285b514e3a4443cced501
young891221 <young891221@gmail.com>
2018-03-07 17:58:30 +0900

    InactiveUserReader 생성

--- src/main/java/com/community/batch/jobs/InactiveUserJob.java
+++ src/main/java/com/community/batch/jobs/InactiveUserJob.java
@@ -1,9 +1,16 @@
 package com.community.batch.jobs;
 
+import com.community.batch.domain.User;
+import com.community.batch.repository.UserRepository;
+
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 /**
  * Created by KimYJ on 2018-03-07.
@@ -17,5 +24,11 @@ public class InactiveUserJob {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
+    @Autowired
+    private UserRepository userRepository;
 
+    @Bean
+    public ItemReader<List<User>> inactiveUserReader() {
+        return () -> userRepository.findAll();
+    }
 }
