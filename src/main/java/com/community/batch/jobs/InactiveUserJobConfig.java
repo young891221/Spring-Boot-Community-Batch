74cd978dd2f3fa35199831ef9e3a5268a73ce629
youngjae <young891221@gmail.com>
2018-04-07 17:39:12 +0900

    .

--- src/main/java/com/community/batch/jobs/InactiveUserJobConfig.java
+++ src/main/java/com/community/batch/jobs/InactiveUserJobConfig.java
@@ -2,28 +2,25 @@ package com.community.batch.jobs;
 
 import com.community.batch.domain.User;
 import com.community.batch.domain.enums.UserStatus;
-import com.community.batch.jobs.common.readers.QueueItemReader;
 import com.community.batch.jobs.inactive.InactiveItemProcessor;
 import com.community.batch.repository.UserRepository;
-
+import lombok.AllArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
-
-import javax.persistence.EntityManagerFactory;
-
-import lombok.AllArgsConstructor;
 
 /**
  * Created by KimYJ on 2018-03-07.
@@ -32,7 +29,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class InactiveUserJobConfig {
 
-    private final QueueItemReader<User> inactiveUserReader;
+    private final ListItemReader<User> inactiveUserReader;
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final UserRepository userRepository;
@@ -58,10 +55,10 @@ public class InactiveUserJobConfig {
 
     @Bean
     @StepScope
-    public QueueItemReader<User> inactiveUserReader(@Value("#{jobParameters[nowDate]}") Date nowDate) {
+    public ListItemReader<User> inactiveUserReader(@Value("#{jobParameters[nowDate]}") Date nowDate) {
         LocalDateTime now = LocalDateTime.ofInstant(nowDate.toInstant(), ZoneId.systemDefault());
-        List<User> oldUsers = userRepository.findByUpdatedDateBeforeAndStatusEquals(now.minusYears(1), UserStatus.ACTIVE);
-        return new QueueItemReader<>(oldUsers);
+        List<User> inactiveUsers = userRepository.findByUpdatedDateBeforeAndStatusEquals(now.minusYears(1), UserStatus.ACTIVE);
+        return new ListItemReader<>(inactiveUsers);
     }
 
     private InactiveItemProcessor inactiveUserProcessor() {
